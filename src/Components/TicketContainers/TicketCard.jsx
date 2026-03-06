7e5f79e56021a8ed7f72337944bc0a5316ab4d64
Sabbirahmed0007 <www.sabbirahmed3071@gmail.com>
2026-03-06 15:50:41 +0600

    updated the ui

--- src/Components/TicketContainers/TicketCard.jsx
+++ src/Components/TicketContainers/TicketCard.jsx
@@ -10,7 +10,7 @@ const TicketCard = ({ ticket, handleTicket }) => {
         <div onClick={()=>handleTicket(ticket)} className='bg-white cursor-pointer text-black shadow-lg p-3 box-border rounded-md font-inter lg:h-44'>
 
             <div className='flex flex-col md:flex-row items-start md:items-center justify-between mb-3'>
-                <h1 className=' font-semibold text-left overflow-auto w-2/3'>{title}</h1>
+                <h1 className=' font-semibold text-left overflow-auto lg:w-2/3'>{title}</h1>
                 <p className={`badge text-xs  badge-md  ${status.toLowerCase() === 'open' ? 'bg-[#B9F8CF] text-[#02A53B]' :'bg-[#F8F3B9] text-[#9C7700]'}  p-2`}> <span className={` p-2 ${status.toLowerCase() === 'open' ? 'bg-[#0B5E06]' :'bg-[#FEBB0C]'}  rounded-full`}></span> {status}</p>
             </div>
             <div className='text-left text-xs text-gray-400 h-10'>
